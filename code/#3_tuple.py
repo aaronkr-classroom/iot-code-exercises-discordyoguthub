f7c94b17d43d35ee5159c2479e64b55649a2b966
discordyoguthub <discordyo8246@gmail.com>
2026-03-23 11:41:36 +0900

    Add files via upload

---
+++ code/#3_tuple.py
@@ -0,0 +1,19 @@
+#3_tuple.py
+#RGB 색상을 표현하는 튜플 예제
+black = (0, 0, 0)
+white = (255, 255, 255) #2의 8승 -1
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0 ,255, 0)
+my_color = (24, 75, 189) ##??? =>
+My_color_2 = (140, 158, 182)
+
+#my_color[2] = 0 #blue 삭제
+my_dolor = (24, 75, 0)
+
+my_colors = [
+    black, white, red, greem, blue, my_color
+    ]
+
+for color in my_colors:
+    print(color)
---
+++ code/#3str.py
@@ -0,0 +1,17 @@
+#3str.py
+
+f= "hong"
+n = '길동'
+fn= f + n
+
+In = len(fn)
+
+#와 출력하기
+qt1 = "It's a beautiful day"
+qt2 = 'He said, "hello World!"'
+qt3 = "She said, \"No, it's not!\""
+qt4 = 'I\'m hungry.',
+qt5 = C:\Windows\Users\Desktop\'
+
+print(1,fn)
+print(f"2 {
