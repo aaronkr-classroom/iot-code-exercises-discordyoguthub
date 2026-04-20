5a2b3377576bd9f5a5b85c3a23ddd593f4443536
2544025 Lee hyoun jin <discordyo8246@gmail.com>
2026-04-20 12:13:55 +0900

    Add files via upload

---
+++ #room_sensor.py
@@ -0,0 +1,60 @@
+#room_sensor.py
+#part 1
+
+class RoomSensor:
+    def_init_(self.name):
+        self.name = name
+        
+    def temperature(self):
+      x = self.temperature
+          
+    def humidity(self):
+        y = self.humidity
+    
+    
+    def light(self):
+        z = self.light
+        
+        
+    x = self.temperature
+    y = self.humidity
+
+    comfort_level = " "
+
+    if 20 <= x <= 26 and 40 <= y <= 60:
+        comfort_level = "Comfortable"
+        
+    elif 30 <= x or 70 <= y:
+        comfort_level = "Warning"
+        
+    else :
+        comfort_level = "Normal"
+
+    z= self.light
+
+    light_status = " "
+
+    if 200 > z:
+        light_status = "Dark"
+
+    elif 200 <= z:
+        light_status = "Bright"
+    
+class Kitchen:
+    def_init_(name,self):
+        def print_info(name,Temperatuure,""):
+            contact_info = f"Sensor:\t{Kichen}\nTemperature:\t{temperature}:\nHumidity:\t{humidity}"
+    print(f"Sensor:\t{Kichen}\nTemperature:\t{temperature}:\nHumidity:\t{humidity")
+    
+class Bedroom:
+    def_init_(name,self):
+        def print_info(name,Temperatuure,""):
+            contact_info = f"Sensor:\t{Bedroom}\nTemperature:\t{temperature}:\nHumidity:\t{humidity}"
+    print(f"Sensor:\t{Bedroom}\nTemperature:\t{temperature}:\nHumidity:\t{humidity")
+
+class Balcony:
+    def_init_(name,self):
+        def print_info(name,Temperatuure,""):
+            contact_info = f"Sensor:\t{Balcony}\nTemperature:\t{temperature}:\nHumidity:\t{humidity}"
+    print(f"Sensor:\t{Balcony}\nTemperature:\t{temperature}:\nHumidity:\t{humidity")
+
